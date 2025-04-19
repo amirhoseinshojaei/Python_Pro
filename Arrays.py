7e08b3b25233a18130ed3ed3e3cfee26b530efd7
AmirhoseinShojaei <127549836+amirhoseinshojaei@users.noreply.github.com>
2025-04-20 00:42:15 +0330

    complete Array section

---
+++ Arrays.py
@@ -0,0 +1,15 @@
+# Arrays
+
+# یک ساختار داده ایی که عناصر هم نوع را در یک متغیر ذخیره میکند
+
+import array
+
+numbers = array.array('i',[1,2,3,4,5])
+
+print(type(numbers),numbers)
+
+
+print(numbers.buffer_info())
+
+
+
